473b0006c2439b25f0eb24038980289b1dee82c3
Reinier Vargas <reinier-32@hotmail.com>
2022-07-26 13:30:41 -0500

    block scope

---
+++ src/scope/re.js
@@ -0,0 +1,28 @@
+//resignacion y redeclaracion 
+var firstName; //undefined
+firstName = 'Oscar';
+console.log(firstName);
+
+var lastName = "david"; //declarar y asignar
+lastName = 'Ana'; //reasignar
+console.log(lastName);
+
+var secondName = 'David'; //declarando / asignando
+var secondName = 'Ana'; // reasignado
+console.log(secondName);
+
+//let 
+let fruit = 'Apple' // declarar y asignar 
+fruit = 'kiwi' //reasignar
+console.log(fruit);
+
+let fruit = 'banana'; //no es posible redeclarar 
+console.log(fruit);
+
+// const
+const animal = 'dog'; //declara y asigna
+animal = 'cat'; //reasignando ERROR
+console.log(animal);
+
+const animal = 'dog'; //asignar da un error
+console.log(animal);
